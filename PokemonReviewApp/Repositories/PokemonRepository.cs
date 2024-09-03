199bf334cb35ab9ef4cb250dcca84c97c2f97868
Raneem <ranyseleem@gmail.com>
2024-09-03 13:57:39 +0300

    create pokemon repository

---
+++ PokemonReviewApp/Repositories/PokemonRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Identity.Client;
+using Pokemon_Review_App.Data;
+using Pokemon_Review_App.Models;
+using PokemonReviewApp.Interfaces;
+
+namespace PokemonReviewApp.Repositories
+{
+    public class PokemonRepository: IPokemonRepository
+    {
+        private readonly  DataContext _context; 
+        public PokemonRepository (DataContext context)
+            {
+                _context = context;
+            }
+        public Pokemon GetPokemonById(int id)
+        {
+            return _context.Pokemons.Where(p => p.Id == id).FirstOrDefault();
+
+        }
+
+        public Pokemon GetPokemonByName(string name)
+        {
+            return _context.Pokemons.Where(p => p.Name == name).FirstOrDefault();
+
+        }
+
+        public decimal GetPokemonRating(int pokemanId)
+        {
+            var review = _context.Reviews.Where(p => p.Pokemon.Id == pokemanId);
+
+            if (review.Count() <= 0)
+                return 0;
+
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokemnId)
+        {
+            return _context.Pokemons.Any(p => p.Id == pokemnId);
+        }
+    }
+}
