5471e07ba898e93639b527329526de175c93353e
Raneem <ranyseleem@gmail.com>
2024-09-03 16:09:02 +0300

    Creating Category Get Requests: interface, repositoyr, mapper, controller

---
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Pokemon_Review_App.Models;
+using PokemonReviewApp.Dtos;
+using PokemonReviewApp.Interfaces;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+            
+        }
+
+        // get categories by id
+        [HttpGet("{categoryId}")]
+        [ProducesResponseType(200, Type = typeof(Category))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCategoryById(int categoryId)
+        {
+            if (!_categoryRepository.CategoriesExists(categoryId))
+                return NotFound();
+
+            var pokemon = _mapper.Map<CategoryDto>(_categoryRepository.GetCategoryByID(categoryId));
+
+            return Ok(pokemon)
+;        }
+        // get categories by name
+         //TODO: need to fetch the id of that name -->> inside the repo not here!!!
+//        [HttpGet("{name}")]  
+//        [ProducesResponseType(200, Type = typeof(Category))]
+//        [ProducesResponseType(400)]
+//        public IActionResult GetCategoryByName(string name)
+//        {
+//            var category = _categoryRepository.GetCategoryByName(name)
+//            //if (!_categoryRepository.CategoriesExists(categoryId))
+//            //    return NotFound();
+
+//            var pokemon = _mapper.Map<CategoryDto>(_categoryRepository.GetCategoryByID(categoryId));
+
+//            return Ok(pokemon)
+//;
+//        }
+        // get all categories
+        [HttpGet]
+        [ProducesResponseType(200, Type= typeof(IEnumerable<Category>))]
+        public IActionResult GetCategories()
+        {
+            var categories = _mapper.Map<List<CategoryDto>>(_categoryRepository.GetCategories());
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(categories);
+        }
+
+    }
+}
---
+++ PokemonReviewApp/Repositories/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Pokemon_Review_App.Data;
+using Pokemon_Review_App.Models;
+using PokemonReviewApp.Interfaces;
+
+namespace PokemonReviewApp.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+
+        private readonly DataContext _context;
+        public CategoryRepository(DataContext context)
+        {
+            _context = context;
+
+        }
+        public bool CategoriesExists(int id)
+        {
+            return _context.Categories.Any(c => c.Id == id);
+          
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _context.Categories.OrderBy(c => c.Name).ToList();
+        }
+
+        public Category GetCategoryByID(int id)
+        {
+            return _context.Categories.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public Category GetCategoryByName(string name)
+        {
+            return _context.Categories.Where(c => c.Name == name).FirstOrDefault();
+        }
+
+        public ICollection<Pokemon> GetPokemonsByCategory(int categoryId)
+        {
+            return _context.PokemonCategories.Where(c => c.CategoryId == categoryId).Select(e => e.Pokemon).ToList();
+        }
+    }
+}
