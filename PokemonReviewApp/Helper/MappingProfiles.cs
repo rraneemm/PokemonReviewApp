95127970a18471dd5147e4a58e21d56510131cd3
Raneem <ranyseleem@gmail.com>
2024-09-03 13:58:14 +0300

    create pokeman mapper

---
+++ PokemonReviewApp/Helper/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Pokemon_Review_App.Models;
+using PokemonReviewApp.Dtos;
+
+namespace PokemonReviewApp.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+        }
+    }
+}
