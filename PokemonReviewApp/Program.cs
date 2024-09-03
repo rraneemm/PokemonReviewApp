a2fdf0ab66b48670ecf5a111134363b1cac478c4
Raneem <ranyseleem@gmail.com>
2024-09-03 13:57:12 +0300

    Enhance DI setup with repositories and AutoMapper

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Pokemon_Review_App.Data;
 using PokemonReviewApp;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddTransient<Seed>();
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
