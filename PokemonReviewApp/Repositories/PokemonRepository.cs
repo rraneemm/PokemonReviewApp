7a280d833f80808a4e0ff82a5e9de40c585dd3ab
Raneem <ranyseleem@gmail.com>
2024-09-04 17:29:11 +0300

    add repositories to all models

--- PokemonReviewApp/Repositories/PokemonRepository.cs
+++ PokemonReviewApp/Repositories/PokemonRepository.cs
@@ -5,13 +5,46 @@ using PokemonReviewApp.Interfaces;
 
 namespace PokemonReviewApp.Repositories
 {
-    public class PokemonRepository: IPokemonRepository
+    public class PokemonRepository : IPokemonRepository
     {
         private readonly  DataContext _context; 
         public PokemonRepository (DataContext context)
             {
                 _context = context;
             }
+
+        public bool CreatePokemon(int ownerId, int categoryId, Pokemon pokemon)
+        {
+            var pokemonOwnerEntity = _context.Owners.Where(o => o.Id == ownerId).FirstOrDefault();
+            var pokeonCategoryEntity = _context.Categories.Where(c => c.Id == categoryId).FirstOrDefault();
+
+            var pokemonOwner = new PokemonOwner()
+            {
+                Owner = pokemonOwnerEntity,
+                Pokemon = pokemon,
+            };
+
+            _context.Add(pokemonOwner);
+
+            var pokemonCategory = new PokemonCategory()
+            {
+                Category = pokeonCategoryEntity,
+                Pokemon = pokemon,
+            };
+
+            _context.Add(pokemonCategory);
+
+            _context.Add(pokemon);
+
+            return Save();
+        }
+
+        public bool DeletePokemon(Pokemon pokemon)
+        {
+            _context.Remove(pokemon);
+            return Save();
+        }
+
         public Pokemon GetPokemonById(int id)
         {
             return _context.Pokemons.Where(p => p.Id == id).FirstOrDefault();
@@ -42,6 +75,18 @@ namespace PokemonReviewApp.Repositories
         public bool PokemonExists(int pokemnId)
         {
             return _context.Pokemons.Any(p => p.Id == pokemnId);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdatePokemon(Pokemon pokemon)
+        {
+            _context.Update(pokemon);
+            return Save();
         }
     }
 }
---
+++ PokemonReviewApp/Repositories/ReviewerRepository.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Pokemon_Review_App.Data;
+using Pokemon_Review_App.Models;
+using PokemonReviewApp.Interfaces;
+
+namespace PokemonReviewApp.Repositories
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public ReviewerRepository(DataContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+
+        }
+        public bool CreateReviewer(Reviewer reviewer)
+        {
+            _context.Add(reviewer);
+            return Save();
+        }
+
+        public bool DeleteReviewer(Reviewer reviewer)
+        {
+            _context.Remove(reviewer);
+            return Save();
+        }
+
+        public Reviewer GetReviewer(int id)
+        {
+            return _context.Reviewers.Where(r => r.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _context.Reviewers.ToList();
+        }
+
+        public ICollection<Review> GetReviewsByReviewerId(int reviewerId)
+        {
+            return _context.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public bool ReviewerExists(int id)
+        {
+            return _context.Reviewers.Any(r => r.Id == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateReviewer(Reviewer reviewer)
+        {
+            _context.Update(reviewer);
+            return Save();
+        }
+    }
+}
